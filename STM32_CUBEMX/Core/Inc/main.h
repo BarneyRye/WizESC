83b0d367a54edd7011575167c88fae8685052583
Barnaby Rye <113546986+BarneyRye@users.noreply.github.com>
2026-05-15 19:23:30 +0100

    Initial commit

---
+++ STM32_CUBEMX/Core/Inc/main.h
@@ -0,0 +1,85 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32g4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define VBAT_ADC_Pin GPIO_PIN_0
+#define VBAT_ADC_GPIO_Port GPIOA
+#define BEMF_A_Pin GPIO_PIN_1
+#define BEMF_A_GPIO_Port GPIOA
+#define BEMF_B_Pin GPIO_PIN_2
+#define BEMF_B_GPIO_Port GPIOA
+#define BEMB_C_Pin GPIO_PIN_3
+#define BEMB_C_GPIO_Port GPIOA
+#define I_A_Pin GPIO_PIN_5
+#define I_A_GPIO_Port GPIOA
+#define I_B_Pin GPIO_PIN_6
+#define I_B_GPIO_Port GPIOA
+#define I_C_Pin GPIO_PIN_7
+#define I_C_GPIO_Port GPIOA
+#define DSHOT_Pin GPIO_PIN_4
+#define DSHOT_GPIO_Port GPIOB
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
